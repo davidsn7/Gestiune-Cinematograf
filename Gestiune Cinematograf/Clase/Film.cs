8f462e071719cb7f88e22c74b88eed8c3f889f9e
liege <liege@DAVIDZEN>
2025-03-01 21:54:52 +0200

    Add project files.

---
+++ Gestiune Cinematograf/Clase/Film.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestiune_Cinematograf.Clase
+{
+    class Film
+    {
+        public int Id { get; set; }
+        public string Titlu { get; set; }
+        public int Durata { get; set; }
+        public string Gen { get; set; }
+        public string DescriereFilm { get; set; }
+
+        public Film(int id, string titlu, int durata, string gen, string descriereFilm)
+        {
+            Id = id;
+            Titlu = titlu;
+            Durata = durata;
+            Gen = gen;
+            DescriereFilm = descriereFilm;
+        }
+
+        public string afiseazaDetalii()
+        {
+            return $"ID: {Id} | Titlu: {Titlu} | Gen: {Gen} | Durata: {Durata} min | Descriere film: {DescriereFilm} ";
+        }
+    }
+}
---
+++ Gestiune Cinematograf/Program.cs
@@ -0,0 +1,25 @@
+﻿using Gestiune_Cinematograf.Clase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestiune_Cinematograf
+{
+    class Program
+    {
+        static void Main()        {
+            Film f1 = new Film(1, "Joker", 122, "Drama", "Arthur Fleck, un clovn de petrecere și un comedian ratat, duce o viață grea alături de mama lui bolnavă. Respins de societate și considerat un ciudat, ajunge să îmbrățișeze haosul din Gotham City.");
+            Sala s1 = new Sala(4, "VIP", 150);
+            Proiectie p1 = new Proiectie(3, f1, s1, "19:30");
+            Rezervare r1 = new Rezervare(29, p1, "Simion David", 22);
+
+            Console.WriteLine(p1.AfiseazaDetalii());
+            Console.WriteLine(r1.AfiseazaDetalii());
+
+
+
+        }
+    }
+}
